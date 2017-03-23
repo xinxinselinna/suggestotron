0bd4524775280372e8cf4b342269e69651cdab54
xinxinselinna <parade_adeline@hotmail.com>
2017-03-23 20:49:01 +0800

    add rest files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
-Rails.application.routes.draw do
 
-  resources :topics
-  root 'topics#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  Rails.application.routes.draw do
+   root 'topics#index'
+   resources :topics do
+     member do
+       post 'upvote'
+     end
+   end
+  end
+  
